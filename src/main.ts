c87bd022b603d5b2ed45ca61a7fa44f120ea97ef
Alae Es-saki <alaeessaki@gmail.com>
2019-06-22 21:55:17 +0100

    styling scrollbar

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import 'hammerjs'; 
-
+import 'simplebar';
+import 'simplebar/dist/simplebar.css';
 if (environment.production) {
   enableProdMode();
 }
