cfc44e335d043023fdb03bd46bd8716740b3604b
KhalilKes <akakhalilyo@gmail.com>
2019-06-30 19:42:48 +0100

    khalil adding scroll event

--- src/app/counter-up-section/counter-up-section.component.ts
+++ src/app/counter-up-section/counter-up-section.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+
 
 @Component({
   selector: 'app-counter-up-section',
@@ -10,7 +11,14 @@ export class CounterUpSectionComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    window.addEventListener('scroll', this.scroll, true);
   }
 
+  scroll() {
+    var coun = document.getElementById("counter");
+    console.log(coun.clientHeight);
+  }
 
 }
+
+
