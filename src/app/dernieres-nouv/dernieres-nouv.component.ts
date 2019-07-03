a6ce8ce5ce2395baedd86f4488337cd66e596727
Alae Es-saki <alaeessaki@gmail.com>
2019-07-03 17:49:42 +0100

    add data binding

--- src/app/dernieres-nouv/dernieres-nouv.component.ts
+++ src/app/dernieres-nouv/dernieres-nouv.component.ts
@@ -6,8 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dernieres-nouv.component.scss']
 })
 export class DernieresNouvComponent implements OnInit {
-
-  constructor() { }
+  images: Array<any> = [];
+  constructor() {
+    this.images = [
+      { name: '../../assets/img1.jpg', color:"rgba(103,55,131)", pos:0, title:"Cérémonie des bénévolats" },
+      { name: '../../assets/img2.jpg' , color:"rgba(25,174,118)", pos:1 ,title: "Atelier de formation greenship"},
+      { name: '../../assets/img3.jpg' , color:"rgba(52,152,219)" , pos:0, title:"Atelier de formation C2C"},
+    ]
+   }
 
   ngOnInit() {
   }
