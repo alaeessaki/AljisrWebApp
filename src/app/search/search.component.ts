e7b199aa9dabb7458563c8d66a4cbd23f7b25ede
Alae Es-saki <alaeessaki@gmail.com>
2019-07-04 13:52:54 +0100

    encapsulation search bar

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class SearchComponent implements OnInit {
 
