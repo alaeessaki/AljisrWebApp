57d517d11d34b5da77f1de6aa1fee2108f3b3d30
Alae Es-saki <alaeessaki@gmail.com>
2019-07-04 14:50:45 +0100

    adding news letter template and style

--- src/app/newsletter-section/newsletter-section.component.ts
+++ src/app/newsletter-section/newsletter-section.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-newsletter-section',
   templateUrl: './newsletter-section.component.html',
-  styleUrls: ['./newsletter-section.component.scss']
+  styleUrls: ['./newsletter-section.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class NewsletterSectionComponent implements OnInit {
 
