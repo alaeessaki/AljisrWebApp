2a1f29b89003420747e5224495918f6058f86cb8
KhalilKes <akakhalilyo@gmail.com>
2019-07-05 16:23:25 +0100

    css carousel not responsive with messy sass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { DernieresNouvComponent } from './dernieres-nouv/dernieres-nouv.componen
 import { EventsComponent } from './events/events.component';
 import { EventsDetailsComponent } from './events-details/events-details.component';
 import { CountUpModule } from 'countup.js-angular2';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
 // pages module
 
 
@@ -27,7 +28,8 @@ import { CountUpModule } from 'countup.js-angular2';
     CounterUpSectionComponent,
     DernieresNouvComponent,
     EventsDetailsComponent,
-    EventsComponent
+    EventsComponent,
+    TestimonialsComponent
   ],
   imports: [
     BrowserModule,
