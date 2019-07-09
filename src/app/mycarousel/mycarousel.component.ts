51023dd3d29bfed5f45693370f9cae1c62537e0d
KhalilKes <akakhalilyo@gmail.com>
2019-07-09 14:31:49 +0100

    adding team aljisr carousel

--- src/app/mycarousel/mycarousel.component.ts
+++ src/app/mycarousel/mycarousel.component.ts
@@ -40,13 +40,14 @@ export class MycarouselComponent implements OnInit {
 
     function movingSl() {
       if (counter >= carouselElements.length - 1) {
-        counter = 1;
+        counter = 0;
       }
       carouselSlide.style.transition = "transform 0.4s ease-in-out";
       counter++;
       carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
     }
     setInterval(movingSl, 3000);
+
     //  btn listener 
     nextBtn.addEventListener('click', () => {
       if (counter >= carouselElements.length - 1) return;
