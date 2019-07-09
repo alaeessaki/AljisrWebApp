0ddb62348a55d200a1487e1dfcae338ed3ec41c2
Alae Es-saki <alaeessaki@gmail.com>
2019-07-09 16:20:29 +0100

    truning binding data

--- src/app/aljisr-platforms/aljisr-platforms.component.ts
+++ src/app/aljisr-platforms/aljisr-platforms.component.ts
@@ -21,4 +21,7 @@ export class AljisrPlatformsComponent implements OnInit {
   ngOnInit() {
   }
 
+  title = "My Jisr";
+  description = "Cette plateforme est principalement à la disposition et au service de tous les acteurs œuvrant à ‘’restaurer’’ et améliorer l’éducation prioritaire, fondamentale. Elle donne de la visibilité à leurs actions en valorisant tout ce qui s’entreprend, se réalise, ou s’envisage au sein de chaque catégorie d’intervenants. En espace de proximité, d’interaction et d’interactivité, elle contribue à la constructivité et à la construction d’un modèle de communication, d’information, de traitement, de suivi et de coordination autour des problématiques et des exigences de l’éducation."
+
 }
