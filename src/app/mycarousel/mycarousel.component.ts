fee5f1e7e834b61170ab5bfab1f9eeafea9ae124
KhalilKes <akakhalilyo@gmail.com>
2019-07-09 13:30:51 +0100

    carouselamos nightmare

--- src/app/mycarousel/mycarousel.component.ts
+++ src/app/mycarousel/mycarousel.component.ts
@@ -27,33 +27,40 @@ export class MycarouselComponent implements OnInit {
   }
 
   ngOnInit() {
-    const carouselSlide:HTMLElement  = document.querySelector('.wraper') ;
-    const carouselElements = document.querySelectorAll('.wraper mat-card') ;
+    const carouselSlide: HTMLElement = document.querySelector('.wraper');
+    const carouselElements = document.querySelectorAll('.wraper mat-card');
     const prevBtn = document.querySelector('#prevBtn');
     const nextBtn = document.querySelector('#nextBtn');
 
 
     let counter = 1;
     let size = carouselElements[0].clientWidth;
-    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'
 
+
+    function movingSl() {
+      if (counter >= carouselElements.length - 1) {
+        counter = 1;
+      }
+      carouselSlide.style.transition = "transform 0.4s ease-in-out";
+      counter++;
+      carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+    setInterval(movingSl, 3000);
     //  btn listener 
     nextBtn.addEventListener('click', () => {
-        if (counter >= carouselElements.length - 1 ) return;
-        carouselSlide.style.transition = "transform 0.4s ease-in-out";
-        counter++;
-        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+      if (counter >= carouselElements.length - 1) return;
+      carouselSlide.style.transition = "transform 0.4s ease-in-out";
+      counter++;
+      carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
     })
     prevBtn.addEventListener('click', () => {
-      if (counter <= 0 ) return;
-        carouselSlide.style.transition = "transform 0.4s ease-in-out";
-        counter--;
-        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+      if (counter <= 0) return;
+      carouselSlide.style.transition = "transform 0.4s ease-in-out";
+      counter--;
+      carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
     })
-
   }
-
-
 }
 
 
--- src/app/team-aljisr/team-aljisr.component.ts
+++ src/app/team-aljisr/team-aljisr.component.ts
@@ -10,6 +10,45 @@ export class TeamAljisrComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    const carouselSlide: HTMLElement = document.querySelector('.wraper');
+    const carouselElements = document.querySelectorAll('.wraper mat-card');
+    const prevBtn = document.querySelector('#prevBtn');
+    const nextBtn = document.querySelector('#nextBtn');
+
+
+    let counter = 1;
+    let size = carouselElements[0].clientWidth;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'
+
+
+    function movingSl() {
+      if (counter >= carouselElements.length - 1) {
+        counter = 1;
+        carouselSlide.style.transition = "transform 0.4s ease-in-out";
+        counter--;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+      };
+      carouselSlide.style.transition = "transform 0.4s ease-in-out";
+      counter++;
+      carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+    }
+    setInterval(movingSl, 3000);
+    //  btn listener 
+    nextBtn.addEventListener('click', () => {
+      if (counter >= carouselElements.length - 1) return;
+      carouselSlide.style.transition = "transform 0.4s ease-in-out";
+      counter++;
+      carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    })
+    prevBtn.addEventListener('click', () => {
+      if (counter <= 0) return;
+      carouselSlide.style.transition = "transform 0.4s ease-in-out";
+      counter--;
+      carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    })
+
   }
 
 }
