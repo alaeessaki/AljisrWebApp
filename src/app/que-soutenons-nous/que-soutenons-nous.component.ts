690282f9c6dbafd902e29e62ba56b5591ee972bf
Alae Es-saki <alaeessaki@gmail.com>
2019-07-12 16:52:45 +0100

    changin icons repos

--- src/app/que-soutenons-nous/que-soutenons-nous.component.ts
+++ src/app/que-soutenons-nous/que-soutenons-nous.component.ts
@@ -9,13 +9,13 @@ export class QueSoutenonsNousComponent implements OnInit {
   sections: Array<any> = [];
   constructor() { 
     this.sections = [
-      {image:'../../assets/internet.png',title:'DEVELOPEMENT DURABLE', description:"Lorem ipsum dolor sit am adipiscing elit, sed do eiusmod tempor incididunt"},
+      {image:'../../assets/icons/internet.png',title:'DEVELOPEMENT DURABLE', description:"Lorem ipsum dolor sit am adipiscing elit, sed do eiusmod tempor incididunt"},
 
-      {image:'../../assets/open-book.png',title:'FORMATION', description:"Lorem ipsum dolor sit am adipiscing elit, sed do eiusmod tempor incididunt"},
+      {image:'../../assets/icons/open-book.png',title:'FORMATION', description:"Lorem ipsum dolor sit am adipiscing elit, sed do eiusmod tempor incididunt"},
 
-      {image:'../../assets/file.png',title:'EDUCATION', description:"Lorem ipsum dolor sit am adipiscing elit, sed do eiusmod tempor incididunt"},
+      {image:'../../assets/icons/file.png',title:'EDUCATION', description:"Lorem ipsum dolor sit am adipiscing elit, sed do eiusmod tempor incididunt"},
 
-      {image:'../../assets/solar-energy.png',title:'Progrès social et innovation', description:"Lorem ipsum dolor sit am adipiscing elit, sed do eiusmod tempor incididunt"}
+      {image:'../../assets/icons/solar-energy.png',title:'Progrès social et innovation', description:"Lorem ipsum dolor sit am adipiscing elit, sed do eiusmod tempor incididunt"}
 
     ]
   }
