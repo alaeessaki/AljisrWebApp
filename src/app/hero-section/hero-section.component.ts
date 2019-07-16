c7df8217b21f9f0da460f7fb6a218aebaef6ba76
Alae Es-saki <alaeessaki@gmail.com>
2019-07-16 16:40:24 +0100

    adding quotes carousel

--- src/app/hero-section/hero-section.component.ts
+++ src/app/hero-section/hero-section.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+
 
 @Component({
   selector: 'app-hero-section',
@@ -7,17 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroSectionComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
 
+  slogons: Array<any> = [
+    "Au service de l'école publique \n depuis 1999",
+    "Aujourd'hui agissons ensemble pour \n l'école de demain"
+  ];
+  
+  index = 0;
+  currentSlogon = this.slogons[this.index];
+
+  ngOnInit() {  
+    let _this = this;
+    function thisBind(){
+      _this.change();
+    }
+    setInterval(thisBind, 3000);
+
+   }
+
+  change() {
+      if(this.index<this.slogons.length -1){
+        this.index ++;
+        this.currentSlogon = this.slogons[this.index];
+      }
+      else{
+        this.index = 0;
+        this.currentSlogon = this.slogons[this.index];
+      }
+  }
+
+  
   isClicked = false;
-  clicked(){
-    if(this.isClicked){
+  clicked() {
+    if (this.isClicked) {
       this.isClicked = false;
     }
-    else{
+    else {
       this.isClicked = true;
     }
   }
