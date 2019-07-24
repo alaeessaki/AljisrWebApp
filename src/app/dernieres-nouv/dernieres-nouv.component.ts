0ea78f1469c76b932d67195058f0e4abf9a7cd0d
Alae Es-saki <alaeessaki@gmail.com>
2019-07-24 17:14:20 +0100

    make the website smaller

--- src/app/dernieres-nouv/dernieres-nouv.component.ts
+++ src/app/dernieres-nouv/dernieres-nouv.component.ts
@@ -6,12 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dernieres-nouv.component.scss']
 })
 export class DernieresNouvComponent implements OnInit {
-  images: Array<any> = [];
+  news: Array<any> = [];
   constructor() {
-    this.images = [
-      { name: '../../assets/img1.jpg', color:"rgba(103,55,131)", pos:0, title:"Cérémonie des bénévolats" },
-      { name: '../../assets/img2.jpg' , color:"rgba(25,174,118)", pos:1 ,title: "Atelier de formation greenship"},
-      { name: '../../assets/img3.jpg' , color:"rgba(52,152,219)" , pos:0, title:"Atelier de formation C2C"},
+    this.news = [
+      {id:1, title:"finished cv jetni project",img:"../../assets/dernieres-nouvelles/Pic1-full.png", description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis maxime sint rerum nihil, eaque vitae est sed aspernatur consequatur officia laudantium dolor, alias nemo obcaecati nisi minima excepturi, exercitationem architecto? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis maxime sint rerum nihil, eaque vitae est sed aspernatur" },
+
+      {id:2, title:"finished cv jetni project",img:"../../assets/dernieres-nouvelles/pic2-full.png",  description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis maxime sint rerum nihil, eaque vitae est sed aspernatur consequatur officia laudantium dolor, alias nemo obcaecati nisi minima excepturi, exercitationem architecto? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis maxime sint rerum nihil, eaque vitae est sed aspernatur "}
     ]
    }
 
