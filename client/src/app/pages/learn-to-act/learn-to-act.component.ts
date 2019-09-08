f2a586bb43b11df563c9caae8839f04a1ce86efc
Alae Es-saki <alaeessaki@gmail.com>
2019-09-08 16:12:44 +0100

    init page

--- client/src/app/pages/learn-to-act/learn-to-act.component.ts
+++ client/src/app/pages/learn-to-act/learn-to-act.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as $ from 'jquery';
+
 @Component({
   selector: 'app-learn-to-act',
   templateUrl: './learn-to-act.component.html',
@@ -8,8 +10,18 @@ import { Component, OnInit } from '@angular/core';
 export class LearnToActComponent implements OnInit {
 
   constructor() { }
+  slogons = [
+    "Nous plaçons la préscolarisation au coeur de l'éducation et de la protection de la petite enfance",
+    "2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas enim ullam voluptatibus quo quasi? Quasi, nesciunt. Pariatur harum sapiente laboriosam vero est corporis cumque, recusandae sequi, architecto quia totam asperiores!",
+    "3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas enim ullam voluptatibus quo quasi? Quasi, nesciunt. Pariatur harum sapiente laboriosam vero est corporis cumque, recusandae sequi, architecto quia totam asperiores!"
+  ];
 
   ngOnInit() {
+    $('img').on("load", function () {
+      $(".loading-screen-wrapper").fadeOut("slow")
+    }
+    );
+
   }
 
 }
