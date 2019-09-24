fda0d668c84ee7341b5392d0b68640d53e10248c
Alae Es-saki <alaeessaki@gmail.com>
2019-09-24 20:09:29 +0100

    finishing page

--- client/src/app/pages/greenchip/greenchip.component.ts
+++ client/src/app/pages/greenchip/greenchip.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as $ from 'jquery';
+
 @Component({
   selector: 'app-greenchip',
   templateUrl: './greenchip.component.html',
@@ -8,8 +10,22 @@ import { Component, OnInit } from '@angular/core';
 export class GreenchipComponent implements OnInit {
 
   constructor() { }
-
+  slogons = [
+    "Nous plaçons la préscolarisation au coeur de l'éducation et de la protection de la petite enfance",
+    "2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas enim ullam voluptatibus quo quasi? Quasi, nesciunt. Pariatur harum sapiente laboriosam vero est corporis cumque, recusandae sequi, architecto quia totam asperiores!",
+    "3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas enim ullam voluptatibus quo quasi? Quasi, nesciunt. Pariatur harum sapiente laboriosam vero est corporis cumque, recusandae sequi, architecto quia totam asperiores!"
+  ];
+  numbers = [
+    { 'icon': '../../../assets/icons/tool.png', 'number': '550+', 'text': ' jeunes Diplômés en “Maintenance informatique” et insérés en entreprise' },
+    { 'icon': '../../../assets/icons/computer.png', 'number': '1500+', 'text': 'appareils réparés et distribués aux écoles publiques et associations au maroc ' },
+    { 'icon': '../../../assets/icons/bolt.png', 'number': '150000+', 'text': 'Articles collectés traités ou reconditionnés et plus 680 tonnes livrés à Managem pour traitement et recyclage' }
+  
+  ]
   ngOnInit() {
+    $('img').on("load", function () {
+      $(".loading-screen-wrapper").fadeOut("slow")
+    }
+    );
   }
 
 }
